772b1c4b8652114ffae23b0fbd686e2f94de0f51
gesparza3 <gesparza3@mail.csuchico.edu>
2018-11-28 19:50:26 +0000

    Added tests for toggleTurn function

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -20,3 +20,25 @@ TEST(TicTacToeBoardTest, unitTestName)
 	ASSERT_TRUE(true);
 }
 */
+
+TEST(TicTacToeBoardTest, toggleX)
+{
+	TicTacToeBoard b;
+	Piece whose_turn = b.toggleTurn();
+	ASSERT_EQ(whose_turn == O, true);
+}
+
+TEST(TicTacToeBoardTest, toggleO)
+{
+	TicTacToeBoard b;
+	Piece whose_turn = b.toggleTurn();
+	whose_turn = b.toggleTurn();
+	ASSERT_EQ(whose_turn == X, true);
+}
+
+TEST(TicTacToeBoardTest, isPlaced)
+{
+	TicTacToeBoard b;
+	Piece whose_turn = b.placePiece(1, 1);
+	
+}
